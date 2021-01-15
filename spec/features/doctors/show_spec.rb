a82380bd954529c3849713ad4cc6c905817e4929
Catherine Dean <catherinemdean15@gmail.com>
2021-01-15 10:16:12 -0700

    add doctor show tests for doctor information and relationships

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'doctor show page' do
+  before :each do
+    @hospital = Hospital.create!(name: "St. Person Hospital")
+    @doctor1 = @hospital.doctors.create!(name: "Dr. Lee", specialty: "Pediatrics", university: "Fancy Pants University")
+    @doctor2 = @hospital.doctors.create!(name: "Dr. Sally", specialty: "Bones", university: "Health University")
+    @patient1 = @doctor1.patients.create!(name: "Catherine", age: 12)
+    @patient2 = @doctor1.patients.create!(name: "Ruby", age: 6)
+    @patient3 = @doctor2.patients.create!(name: "Tina", age: 25)
+
+    visit doctor_path(@doctor1)
+  end
+
+  it "shows the doctor's information" do
+    expect(page).to have_content(@doctor1.name)
+    expect(page).to have_content(@doctor1.specialty)
+    expect(page).to have_content(@doctor1.university)
+
+    expect(page).to_not have_content(@doctor2.name)
+  end
+
+  it "shows the name of the hospital and patients associated" do
+    expect(page).to have_content(@hospital.name)
+    expect(page).to have_content(@patient1.name)
+    expect(page).to have_content(@patient2.name)
+
+    expect(page).to_not have_content(@patient3.name)
+  end
+
+
+end
