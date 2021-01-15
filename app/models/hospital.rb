ddfe6f3a6e320ad12b8c197ad61c16fb91b2025d
Catherine Dean <catherinemdean15@gmail.com>
2021-01-15 10:37:33 -0700

    create hospital methods number_of_doctors and unique_universities

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
-end 
+
+  def unique_universities
+    doctors.pluck(:university).uniq
+  end
+
+  def number_of_doctors
+    doctors.count
+  end
+end
