b8cb2a0162655b2ecaa301e9958973eec6babe81
Catherine Dean <catherinemdean15@gmail.com>
2021-01-15 12:10:36 -0700

    create active_patients method

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,8 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :doctors_patients
   has_many :patients, through: :doctors_patients
+
+  def active_patients
+    patients.joins(:doctors_patients).where("doctors_patients.status = 1")
+  end
 end
