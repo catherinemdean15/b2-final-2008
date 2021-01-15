c1f7e02bb6f8e1ddf7d2925400ffa0c7f88f7187
Catherine Dean <catherinemdean15@gmail.com>
2021-01-15 12:09:44 -0700

    add update action to doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,22 @@
 class DoctorsController < ApplicationController
+  before_action :find_doctor
   def show
+
+  end
+
+  def update
+    relationship = DoctorsPatient.where(doctor_id: params[:id], patient_id: patient_id[:patient]).first
+    relationship.update_attributes(:status => 'inactive')
+    redirect_to doctor_path(@doctor)
+  end
+
+
+  private
+  def find_doctor
     @doctor = Doctor.find(params[:id])
   end
 
+  def patient_id
+    params.require(:doctor).permit(:patient)
+  end
 end
